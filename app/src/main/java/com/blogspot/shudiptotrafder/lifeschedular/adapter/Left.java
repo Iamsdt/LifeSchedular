0ac0f53527d2c8aee146e6f8489a7ddb2470d50b
Shudipto Trafder <Shudiptotrafder@gmail.com>
2017-04-18 12:28:11 +0600

    add activity added polish

---
+++ app/src/main/java/com/blogspot/shudiptotrafder/lifeschedular/adapter/Left.java
@@ -0,0 +1,17 @@
+package com.blogspot.shudiptotrafder.lifeschedular.adapter;
+
+/**
+ * LifeSchedular
+ * com.blogspot.shudiptotrafder.lifeschedular.adapter
+ * Created by Shudipto Trafder on 4/18/2017 at 12:25 PM.
+ * Don't modify without permission of Shudipto Trafder
+ */
+
+public class Left {
+    // all left
+    //Activity
+    //Everyday list
+
+    //services
+    //that's add all everyday activity on false
+}
